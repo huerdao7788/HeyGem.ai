aae5ae9599a80daf04f1c4ca8c6823521d1b8dbb
it00021hot <liu15077731547@gmail.com>
2025-03-24 16:30:48 +0800

    改造web版本

--- backup/src/main/config/config.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-export const serviceUrl = {
-  gateway: "http://120.236.203.50:38000"
-}
--- backup/src/main/service/index.js
+++
@@ -1,8 +0,0 @@
-import { init as videoResult } from './video.js'
-import { init as model } from './model.js'
-import { init as voice } from './voice.js'
-export function registerHandler() {
-  videoResult()
-  model()
-  voice()
-}
